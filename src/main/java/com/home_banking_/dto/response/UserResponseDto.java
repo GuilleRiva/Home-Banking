a7288816251e60d2ad765e686c2e3822b9a9ad15
Guille Rivadeneira <guillerivadeneira7@gmail.com>
2026-02-16 23:39:16 -0300

    Resolucion del error de inicio causado por proyeccion JPQL no valida y refactoriza la capa de servicio para una asignacion DTO adecuada. query y metodo findAllAsDto() removido correccion nomenclatura DTO se elimino el uso inestable de la conversion JPQL refactorizacion UserService para asignar entidades DTOs se corrigio la falta de coincidencia de tipos genericos en el controller Inicio de Docker con exito y funcionalidad Swagger

--- src/main/java/com/home_banking_/dto/response/UserResponseDto.java
+++ src/main/java/com/home_banking_/dto/response/UserResponseDto.java
@@ -3,7 +3,6 @@ package com.home_banking_.dto.ResponseDto;
 import com.home_banking_.enums.Rol;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -28,14 +27,11 @@ public class UserResponseDto {
     @Email(message = "Email invalid format")
     private String email;
 
-    @Schema(description = "Password for user authentication. Must be at least 8 characters.", example = "StrongPass123")
-    @Size(min = 8, message = "Password must be at least 8 characters")
-    private String password;
 
     @Schema(description = "User register date", example = "2025-07-08T14:35:00")
     private LocalDateTime registrationDate;
 
     @Schema(description = "User role in the system. Possible values: CLIENT, VENDEDOR", example = "CLIENT", implementation = Rol.class)
-    private Rol rol;
+    private String rol;
 
 }
