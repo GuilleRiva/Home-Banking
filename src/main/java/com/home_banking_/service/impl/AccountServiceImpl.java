af072799be754ca215cdb985f5c309374d2b0056
Guille Rivadeneira <guillerivadeneira7@gmail.com>
2026-03-16 18:50:14 -0300

    mejoras en la capa de service AccountServiceImpl transaccionalidad, logs mas consistentes y con mejoras en su formato, auditoria en metodos claves se reemplazo el metodo de eliminacion de cuentas, por cierre de cuentas mejora en la seguridad de los metodos

--- src/main/java/com/home_banking_/service/impl/AccountServiceImpl.java
+++ src/main/java/com/home_banking_/service/impl/AccountServiceImpl.java
@@ -45,6 +45,9 @@ public class AccountServiceImpl implements AccountService {
     public AccountResponseDto createAccount(AccountCreateRequestDto dto) {
         String email = currentUserService.getCurrentUserEmail();
 
+        log.info("[ACCOUNT_CREATE_INIT] userEmil={} alias={} accountType={}",
+                email, dto.getAlias(), dto.getTypeAccount());
+
         Users users = usersRepository.findByEmail(email)
                 .orElseThrow(() -> new ResourceNotFoundException("User not found"));
 
@@ -61,49 +64,77 @@ public class AccountServiceImpl implements AccountService {
 
         accountRepository.save(account);
 
+        auditLogService.registerEvent(
+                users.getId(),
+                "Account created successfully. accountId=" + account.getId()
+                + ", alias=" + account.getAlias()
+                + ", accountType=" + account.getTypeAccount(),
+                "CREATE_ACCOUNT",
+                "BANKING"
+        );
+
+        log.info("[ACCOUNT_CREATE_SUCCESS] userEmail={} accountId={} alias={} accountType={}",
+                email, account.getId(), account.getAlias(), account.getTypeAccount());
+
         return accountMapper.toDto(account);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<AccountResponseDto> getAll() {
+        String adminEmail= currentUserService.getCurrentUserEmail();
+
+        log.info("[ACCOUNT_FETCH_ALL_INIT] adminEmail={}", adminEmail);
 
         List<AccountResponseDto> accounts = accountRepository.findAll()
                 .stream()
                 .map(accountMapper::toDto)
                 .toList();
 
-        log.debug("Total accounts found: {}", accounts.size());
+        log.debug("[ACCOUNT_FETCH_ALL_SUCCESS] adminEmail={} accountSize={}",adminEmail, accounts.size());
         return accounts;
     }
 
     @Override
+    @Transactional(readOnly = true)
     public AccountResponseDto getAccountById(Long id) {
+        String email = currentUserService.getCurrentUserEmail();
 
-        Account account = accountRepository.findById(id)
-                .orElseThrow(()->  new ResourceNotFoundException(
-                        "Account not found"
-                ));
+        log.info("[ACCOUNT_FETCH_BY_ID_INIT] userEmail={} accountId={}", email, id);
+
+        Account account = getOwnedAccountForUpdate(id,email);
+
+        log.info("[ACCOUNT_FETCH_BY_ID_SUCCESS] userEmail={} accountId{} status={}",
+                email, account.getId(), account.getStatusAccount());
         return accountMapper.toDto(account);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public BigDecimal getBalance(Long accountId) {
+        String email = currentUserService.getCurrentUserEmail();
 
-      Account account = accountRepository.findById(accountId)
-              .orElseThrow(()-> new ResourceNotFoundException(
-                      "Account not found"
-              ));
+        log.info("[ACCOUNT_BALANCE_FETCH_INIT] userEmail={} accountId={}", email,accountId);
 
-      log.debug("Current account balance {}: {}", account, account.getBalance());
-      return account.getBalance();
+        Account account = getOwnedAccount(accountId, email);
+
+        log.info("[ACCOUNT_BALANCE_FETCH_SUCCESS] userEmail={} accountId={} balance={}",
+                email, account.getId(), account.getBalance());
+
+        return account.getBalance();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public AccountResponseDto getAccountByAlias(String alias) {
+        String email = currentUserService.getCurrentUserEmail();
         String maskedAlias = maskAlias(alias);
-        log.info("Searching account with alias: {}", maskedAlias);
+
+        log.info("[ACCOUNT_FETCH_BY_ALIAS_INIT] userEmail={} alias={}", email, maskedAlias);
 
        Account account =getOwnedAccountByAlias(alias, maskedAlias);
+       log.info("[ACCOUNT_FETCH_BY_ALIAS_SUCCESS] userEmail={} accountId={} alias={}",
+               email, account.getId(), maskedAlias);
 
        return accountMapper.toDto(account);
     }
@@ -112,26 +143,41 @@ public class AccountServiceImpl implements AccountService {
     @Override
     public void deleteAccount(Long id) {
         String email = currentUserService.getCurrentUserEmail();
+        log.info("[ACCOUNT_CLOSE_INIT] userEmail={} accountId={}", email, id);
 
-        log.info("Account closure request with ID: {}", id);
-
-        Account account = accountRepository.findByAliasAndUsersEmail(String.valueOf(id),email)
-                .orElseThrow(()-> new ResourceNotFoundException( "Account not found"));
+        Account account = getOwnedAccountForUpdate(id, email);
 
         validateAccountCanBeClosedOrDeleted(account);
-        account.setStatusAccount(StatusAccount.CLOSED);
-
-        accountRepository.save(account);
-        log.info("Account successfully deleted with ID: {}", id);
 
         auditLogService.registerEvent(
                 account.getUsers().getId(),
-                "Account closed successfully. accountId=" + account.getId() + ", alias=" + account.getAlias(),
+                "Account closure rejected because is greater tha zero. accountId=" + account.getId()
+                + ", balance=" + account.getBalance(),
+                "CLOSE_ACCOUNT_REJECTED",
+                "BANKING"
+        );
+
+        if (account.getStatusAccount() == StatusAccount.CLOSED) {
+            throw new BusinessException("Account is already closed");
+        }
+        log.warn("[ACCOUNT_CLOSE_REJECTED] Account is already closed. userEmail={} accountId={}",
+                email, account.getId());
+
+        account.setStatusAccount(StatusAccount.CLOSED);
+        accountRepository.save(account);
+
+        auditLogService.registerEvent(
+                account.getUsers().getId(),
+                "Account closed successfully. accountId=" + account.getId()
+                        + ", alias=" + account.getAlias()
+                        + ", status=" + account.getStatusAccount(),
                 "CLOSE_ACCOUNT",
                 "BANKING"
         );
-        log.info("Account successfully closed with ID: {}", id);
+       log.info("[ACCOUNT_CLOSE_SUCCESS] userEmail={} accountId={} status={}",
+               email, account.getId(), account.getStatusAccount());
     }
+
     // HELPERS //
     private void validateAlias(String alias) {
         if (alias == null || alias.isBlank()) {
@@ -165,10 +211,21 @@ public class AccountServiceImpl implements AccountService {
         if (account.getBalance().compareTo(BigDecimal.ZERO) > 0) {
             throw new BusinessException("Account cannot be deleted while balance is greater than zero");
         }
+        auditLogService.registerEvent(
+                account.getUsers().getId(),
+                "Account closure rejected because account has an active loan. accountId=" + account.getId(),
+                "CLOSE_ACCOUNT_REJECTED",
+                "BANKING"
+        );
     }
 
     private Account getOwnedAccount(Long accountId, String email) {
         return accountRepository.findByIdAndUsersEmail(accountId, email)
+                .orElseThrow(() -> new ResourceNotFoundException("Account not found"));
+    }
+
+    private Account getOwnedAccountForUpdate(Long accountId, String email) {
+        return accountRepository.findByIdAndUsersEmailForUpdate(accountId, email)
                 .orElseThrow(()-> new ResourceNotFoundException("Account not found"));
     }
 
